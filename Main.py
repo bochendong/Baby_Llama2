8d9fa62316bb61f2a6677df1a6e1dd64e3888efe
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-07-04 03:06:57 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -136,12 +136,13 @@ def train(rank, num_gpus, config):
 if __name__ == '__main__':
     dir_path = '/lustre/orion/bif146/world-shared/enzhi/baby_llama/Baby_Llama2'
 
+    '''
     config = read_config()
 
     if config["Preprocess"] == True:
         DataPreProcess()
     
-    '''
+
     setup_logging(dir_path + "/Log/training.log")
     '''
 
